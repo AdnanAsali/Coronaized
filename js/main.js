d93f5065204265fdb71775329aee79575ea18b3b
Bara Abraham <barasec@protonmail.com>
2020-03-09 15:34:26 +0200

    Questions Added

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ var questionsArray =
 [
     {
         "id": 1,
-        "Qtext": "DO YOU WASH YOUR HANDS ?",
+        "Qtext": "HOW OLD ARE YOU?",
         "ans": 
         [
-            "YES",
-            "NO",
+            "YOUNGER THAN 40",
+            "OLDER THAN 40",
         ]
     },
     {
         "id": 2,
-        "Qtext": "DO YOU LIVE IN ITALY OR CHINA ?",
+        "Qtext": "DO YOU LIVE IN CHINA?",
         "ans": 
         [
             "YES",
@@ -20,31 +20,184 @@ var questionsArray =
     },
     {
         "id": 3,
-        "Qtext": "HOW MANY TIMES YOU WASH YOUR HANDS PER DAY ?",
+        "Qtext": "HOW MANY TIMES DO YOU WASH YOUR HANDS WITH SOAP PER DAY?",
         "ans": 
         [
-            1,
-            2,
-            3
+            "1 OR 2 TIMES",
+            "3 TO 5 TIMES",
+            "MORE THAN 5 TIMES"
         ]
     },
     {
         "id": 4,
-        "Qtext": "HOW OLD ARE YOU?",
+        "Qtext": "HOW LONG DO YOU WASH YOUR HANDS FOR?",
         "ans": 
         [
-            1,
-            2,
-            3
+            "LESS THAN 10 SECONDS",
+            "10 TO 20 SECONDS",
+            "20 SECONDS OR MORE"
         ]
     },
     {
         "id": 5,
-        "Qtext": "DO YOU WORK IN A PLACE FULL OF PEOPLE ?",
+        "Qtext": "DO YOU USE AN ALCOHOL-BASED HAND SANITIZER THROUGHOUT THE DAY?",
         "ans": 
         [
             "YES",
             "NO",
+        ]
+    },
+    {
+        "id": 6,
+        "Qtext": "DO YOU SHAKE HANDS, HUG, OR KISS PEOPLE?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 7,
+        "Qtext": "DO YOU SHAKE HANDS, HUG, OR KISS PEOPLE?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 8,
+        "Qtext": "DO YOU WEAR A MASK WITH FFP2 OR FFP3 BASED FILTER?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 9,
+        "Qtext": "DO YOU KEEP AT LEAST ONE METER AWAY FROM OTHER PEOPLE?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 10,
+        "Qtext": "DO YOU TOUCH YOUR EYES, NOSE OR MOUTH WITH YOUR HANDS?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 11,
+        "Qtext": "DO YOU COVER YOUR MOUTH AND NOSE WHEN YOU SNEEZE OR COUGH?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 12,
+        "Qtext": "DO YOU COVER YOUR MOUTH AND NOSE WHEN YOU SNEEZE OR COUGH?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 13,
+        "Qtext": "DO YOU CLEAN THE SURFACES WITH CHLORINE DISINFECTANTS OR ALCOHOL-BASED PRODUCTS?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 14,
+        "Qtext": "DID YOU HAVE A RECENT TRIP TO CHINA?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 15,
+        "Qtext": "HAVE YOU BEEN IN CONTACT WITH PEOPLE BACK FROM CHINA?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 16,
+        "Qtext": "DO YOU WEAR OVERLAPPING MASKS?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 17,
+        "Qtext": "WHEN YOU COUGH, DO YOU COVER YOUR MOUTH WITH YOUR HANDS?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 18,
+        "Qtext": "WHEN YOU COUGH, DO YOU COUGH INTO YOUR ELBOW?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 19,
+        "Qtext": "WHEN YOU COUGH, DO YOU COUGH INTO A DISPOSABLE HANDKERCHIEF?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 20,
+        "Qtext": "DO YOU WASH YOUR HANDS WITH SOAP AFTER CONTACT WITH PETS/ANIMALS?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 21,
+        "Qtext": "DO YOU HAVE FEVER, COUGH, DIFFICULTY BREATHING, MUSCLE ACHES, OR FATIGUE?",
+        "ans": 
+        [
+            "YES",
+            "NO",
+        ]
+    },
+    {
+        "id": 22,
+        "Qtext": "IF ANSWERED YES, HOW WOULD YOU RATE THEIR INTENSITY?",
+        "ans": 
+        [
+            "NORMAL",
+            "STRONGER THAN USUAL",
         ]
     },
 ];
